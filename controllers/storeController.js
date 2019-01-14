877f0e9e1107f4525a3c499e7de5cf7494b2f374
Alexandr Ketov <ketov.alex.v@gmail.com>
2019-01-14 12:56:10 +0600

    Flash messages.

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -11,7 +11,8 @@ exports.addStore = (req, res) => {
 };
 
 exports.createStore = async (req, res) => {
-  const store = new Store(req.body);
-  await store.save();
-  res.redirect('/');
+  const store = await (new Store(req.body)).save();
+  // Works only with sessions
+  req.flash('success', `Successfully created ${store.name}. Care to leave a review.`);
+  res.redirect(`/store/${store.slug}`);
 };
